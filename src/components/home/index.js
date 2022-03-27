56364a3e8c96944d8cb69aa95ebf13694dc7f302
Feliiks <sobrero.ludovic@gmail.com>
2022-03-27 11:12:44 +0200

    Edit: videos responsive

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,28 +3,41 @@ import ReactPlayer from 'react-player'
 
 import first_video from "../../assets/first_video.mp4"
 import second_video from "../../assets/second_video.m4v"
+import first_video_mobile from "../../assets/first_video_mobile.m4v"
+import second_video_mobile from "../../assets/second_video_mobile.m4v"
 
 import Preloader from "../preloader"
 
 const Index = () => {
 
-  const videoRef = useRef()
+  const firstVideoRef = useRef()
+  const secondVideoRef = useRef()
+  const screenWidth = window.screen.width
+
+  const playSecondVideo = () => {
+    firstVideoRef.current.classList.remove("playing")
+    secondVideoRef.current.classList.add("playing")
+    secondVideoRef.current.play()
+  }
 
   useEffect(() => {
     let timer = setTimeout(() => {
-      videoRef.current.play()
+      firstVideoRef.current.play()
     }, 3500)
 
     return () => {
       clearTimeout(timer)
     }
-  }, [])
+  }, [screenWidth])
 
   return (
     <div className="home container-fluid">
       <Preloader />
-      <video className='bgVid' width="100%" muted ref={videoRef}>
-        <source src={first_video} type='video/mp4' />
+      <video className='bgVid playing' ref={firstVideoRef} muted onEnded={() => playSecondVideo()}>
+        <source src={screenWidth > 730 ? first_video : first_video_mobile} type='video/mp4' />
+      </video>
+      <video className='bgVid' ref={secondVideoRef} muted loop>
+        <source src={screenWidth > 730 ? second_video : second_video_mobile} type='video/mp4' />
       </video>
     </div>
   );
